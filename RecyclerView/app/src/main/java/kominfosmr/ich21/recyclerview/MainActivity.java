7f213d41b172ed2d31130e5a294c045cf41def35
ichsan21 <ichsanul0411@gmail.com>
2019-12-17 10:58:19 +0800

    First Commit

---
+++ RecyclerView/app/src/main/java/kominfosmr/ich21/recyclerview/MainActivity.java
@@ -0,0 +1,92 @@
+package kominfosmr.ich21.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(getListHeroes());
+        showRecylclerList();
+    }
+
+    public ArrayList<Hero> getListHeroes() {
+        String[] dataName = getResources().getStringArray(R.array.data_name);
+        String[] dataDescription = getResources().getStringArray(R.array.data_description);
+        String[] dataPhoto = getResources().getStringArray(R.array.data_photo);
+
+        ArrayList<Hero> listHero = new ArrayList<>();
+        for (int i = 0; i < dataName.length; i++){
+            Hero hero = new Hero();
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            hero.setPhoto(dataPhoto[i]);
+
+            listHero.add(hero);
+        }
+        return listHero;
+    }
+
+    private void showRecylclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+    private void showRecyclerGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemsSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode){
+        switch (selectedMode){
+            case R.id.action_list:
+                showRecylclerList();
+                break;
+
+            case R.id.action_grid:
+                showRecyclerGrid();
+                break;
+
+            case R.id.action_cardview:
+                showRecyclerCardView();
+                break;
+        }
+    }
+}
